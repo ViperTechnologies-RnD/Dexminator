c67a6b640e4b9525735def86927f89a546550754
Sobhan Bahrami <ednqor@inf.elte.hu>
2022-02-21 13:44:32 +0100

    Update dex.js adding listing modals selection

--- dex.js
+++ dex.js
@@ -1,6 +1,44 @@
-Moralis.initialize(""); // Application id from moralis.io
+Moralis.initialize("");
 Moralis.serverURL = "";
 
+let currentTrade = {};
+let currentSelectSide;
+let tokens;
+
+async function initialize() {
+    await Moralis.initPlugins();
+    await Moralis.enableWeb3();
+    await listAvailableTokens(); 
+}
+async function listAvailableTokens() {
+    const result = await Moralis.Plugins.oneInch.getSupportedTokens({
+        chain: 'eth',
+    });
+    tokens = result.tokens;
+    let parent = document.getElementById("token_list");
+    for ( const address in tokens) {
+        let token = tokens[address];
+        let div = document.createElement("div");
+        div.setAttribute("data-address", address);
+        div.className = "token_row";
+        let html = `
+        <img class="token_list_img" src="${token.logoURI}">
+        <span class="token_list_text">${token.symbol}</span> 
+        `
+        div.innerHTML = html;
+        div.onclick = selectToken;
+        parent.appendChild(div);
+    }
+    //console.log(tokens);
+}
+
+async function selectToken() {
+    closeModal();
+    let address = event.target.getAttribute("data-address");
+    //console.log(address);
+    currentTrade[currentSelectSide] = tokens[address];
+    console.log(currentTrade);
+}
 async function login() {
     try {
         currentUser = Moralis.User.current();
@@ -12,15 +50,18 @@ async function login() {
     }
 }
 
-function displayModal() {
+function displayModal(side) {
+    currentSelectSide = side;
     document.getElementById("token_modal").style.display = "block";
 }
 
-function modal_close() {
-    document.getElementById("token_modal").style.display = "block";
+function closeModal() {
+    document.getElementById("token_modal").style.display = "none";
 }
 
-
+initialize();
 document.getElementById("modal_close").onclick = closeModal;
-document.getElementById("from_token_select").onclick = displayModal;
+document.getElementById("from_token_select").onclick = (() => {
+    displayModal("from")
+});
 document.getElementById("login_button").onclick = login;
